7b6f85edc8c9bc3e0041f3e4aec4e39188449921
Timur Gamkrelidze <gtd@ir-bis.com>
2025-05-17 21:31:28 +0400

    fix: add more env variables for bd

--- src/bd.ts
+++ src/bd.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise"
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
